4b027fd9625285e955db5b07bb80aeb412b1cf64
Ben <benjamin.guenoun@le-campus-numerique.fr>
2024-11-19 15:15:22 +0100

    Iteration 4 – Conditions d’arrêt du jeu

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -17,6 +17,25 @@ public class TicTacToe {
         scanner = new Scanner(System.in);
     }
 
+    private void displayGameTitle() {
+        // Codes de couleur ANSI
+        final String RESET = "\u001B[0m";
+        final String CYAN = "\u001B[36m";
+        final String YELLOW = "\u001B[33m";
+        final String BLUE = "\u001B[34m";
+        final String BOLD = "\u001B[1m";
+
+        System.out.println(CYAN + BOLD + "********************************" + RESET);
+        System.out.println(CYAN + BOLD + "*                              *" + RESET);
+        System.out.println(CYAN + BOLD + "*        " + YELLOW + "TIC-TAC-TOE" + CYAN + "           *" + RESET);
+        System.out.println(CYAN + BOLD + "*         " + BLUE + "Version 1.0" + CYAN + "          *" + RESET);
+        System.out.println(CYAN + BOLD + "*                              *" + RESET);
+        System.out.println(CYAN + BOLD + "********************************" + RESET);
+        System.out.println(YELLOW + "Joueur 1: X  |  Joueur 2: O" + RESET);
+        System.out.println(BLUE + "Entrez les coordonnées (1-3) pour jouer" + RESET);
+        System.out.println();
+    }
+
     private void initializeBoard() {
         for (int i = 0; i < SIZE; i++) {
             for (int j = 0; j < SIZE; j++) {
@@ -26,6 +45,8 @@ public class TicTacToe {
     }
 
     public void play() {
+        displayGameTitle();
+
         while (!isGameOver()) {
             playTurn();
         }
@@ -47,7 +68,7 @@ public class TicTacToe {
         setOwner(move[0], move[1], currentPlayer);
         switchPlayer();
     }
-
+    // Affiche le plateau de jeu
     private void display() {
         System.out.println("-------------");
         for (Cell[] row : board) {
@@ -55,7 +76,7 @@ public class TicTacToe {
             System.out.println("-------------");
         }
     }
-
+    // Affiche une ligne du plateau
     private void displayRow(Cell[] row) {
         System.out.print("|");
         for (Cell cell : row) {
@@ -65,14 +86,14 @@ public class TicTacToe {
     }
 
     private int[] getMoveFromPlayer() {
-        int[] move = new int[2];
+        int[] move = new int[2]; //Tableau de
         boolean validMove = false;
 
         while (!validMove) {
-            System.out.println("Entrez le numéro de ligne (0 à 2) et la colonne (0 à 2) séparés par un espace :");
+            System.out.println("Entrez le numéro de ligne (1 à 3) et la colonne (1 à 3) séparés par un espace :");
             try {
-                move[0] = scanner.nextInt();
-                move[1] = scanner.nextInt();
+                move[0] = scanner.nextInt() - 1;
+                move[1] = scanner.nextInt() - 1;
 
                 if (isValidMove(move[0], move[1])) {
                     validMove = true;
@@ -100,7 +121,7 @@ public class TicTacToe {
     }
 
     private void switchPlayer() {
-        currentPlayer = (currentPlayer == player1) ? player2 : player1;
+        currentPlayer = (currentPlayer == player1) ? player2 : player1; // Condition vrai ? faux :
     }
 
     private boolean isGameOver() {
@@ -108,7 +129,6 @@ public class TicTacToe {
     }
 
     private boolean isWinner() {
-        // Vérifier les lignes, colonnes et diagonales
         for (int i = 0; i < SIZE; i++) {
             if (checkLine(i, 0, 0, 1) || checkLine(0, i, 1, 0)) {
                 return true;
